f60cce09ba2deeb48be8d59da6c7b571f3e973ef
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-07-31 11:22:17 +0400

    :test_tube: :white_check_mark: Adds tests

--- src/auth/auth.test.ts
+++ src/auth/auth.test.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import { describe, expect, it } from 'vitest';
-import { refreshToken } from './';
+import { refreshToken, tokensFromCode } from './';
 
 describe('AUTH Helpers', () => {
   it('should refresh access token', async () => {
@@ -9,5 +9,12 @@ describe('AUTH Helpers', () => {
     const tokens = await refreshToken(refresh);
     expect(tokens).toHaveProperty('access_token');
     expect(tokens).toHaveProperty('expires_in');
+  });
+  it('should throw on invalid refresh token', async () => {
+    await expect(() => refreshToken('invalid')).rejects.toThrow();
+  });
+  /* MYSTERY: How do we do a test of a valid code when codes easily expire? */
+  it('should throw when given invalid code', async () => {
+    await expect(() => tokensFromCode('invalid')).rejects.toThrow();
   });
 });
