d04db4777e42e30d368d65db5f95766c21b6057c
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-08-06 12:17:23 +0400

    :fire: Removes left over files

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
 export { refreshToken } from './auth/refreshToken';
 export { tokensFromCode } from './auth/tokensFromCode';
-export { SPOTIFY_URL } from './constants';
+export { SPOTIFY_URL, SPOTIFY_AUTH } from './constants';
 export { resetCache } from './core/resetCache';
 export { setToken } from './core/setToken';
 export { spotifyApiClient } from './core/spotifyApiClient';
 export { getCurrentUser } from './endpoints/users/getCurrentUser';
-export { deepFreeze } from './utils/deepFreeze';
-export { isBrowser, isNode } from './utils/isBrowserOrNode';
-export { sleep } from './utils/sleep';
 export { spotifyFetch } from './utils/spotifyFetch';
-export { toBase64 } from './utils/toBase64';
 export type { RefreshedToken } from './auth/refreshToken';
 export type { SpotifyTokens } from './auth/tokensFromCode';
 export type {
