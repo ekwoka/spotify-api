556e4be6359265a6a8bd3a943e6e393adf2c10aa
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-08-06 11:59:01 +0400

    :rotating_light: lints

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,5 +1,5 @@
-export { fetchOptions } from "./fetchOptions";
-export { refreshToken } from "./refreshToken";
-export { tokensFromCode } from "./tokensFromCode";
-export type { RefreshedToken } from "./refreshToken";
-export type { SpotifyTokens } from "./tokensFromCode";
+export { fetchOptions } from './fetchOptions';
+export { refreshToken } from './refreshToken';
+export { tokensFromCode } from './tokensFromCode';
+export type { RefreshedToken } from './refreshToken';
+export type { SpotifyTokens } from './tokensFromCode';
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
-export { refreshToken } from "./auth/refreshToken";
-export { tokensFromCode } from "./auth/tokensFromCode";
-export { SPOTIFY_URL } from "./constants";
-export { resetCache } from "./core/resetCache";
-export { setToken } from "./core/setToken";
-export { spotifyApiClient } from "./core/spotifyApiClient";
-export { getCurrentUser } from "./endpoints/users/getCurrentUser";
-export { deepFreeze } from "./utils/deepFreeze";
-export { isBrowser, isNode } from "./utils/isBrowserOrNode";
-export { sleep } from "./utils/sleep";
-export { spotifyFetch } from "./utils/spotifyFetch";
-export { toBase64 } from "./utils/toBase64";
-export type { RefreshedToken } from "./auth/refreshToken";
-export type { SpotifyTokens } from "./auth/tokensFromCode";
+export { refreshToken } from './auth/refreshToken';
+export { tokensFromCode } from './auth/tokensFromCode';
+export { SPOTIFY_URL } from './constants';
+export { resetCache } from './core/resetCache';
+export { setToken } from './core/setToken';
+export { spotifyApiClient } from './core/spotifyApiClient';
+export { getCurrentUser } from './endpoints/users/getCurrentUser';
+export { deepFreeze } from './utils/deepFreeze';
+export { isBrowser, isNode } from './utils/isBrowserOrNode';
+export { sleep } from './utils/sleep';
+export { spotifyFetch } from './utils/spotifyFetch';
+export { toBase64 } from './utils/toBase64';
+export type { RefreshedToken } from './auth/refreshToken';
+export type { SpotifyTokens } from './auth/tokensFromCode';
 export type {
-	PersistentApiProperties,
-	SpotifyApiClient,
-	QueryFunction,
-	QueryConstructor
-} from "./core/types";
+  PersistentApiProperties,
+  SpotifyApiClient,
+  QueryFunction,
+  QueryConstructor,
+} from './core/types';
