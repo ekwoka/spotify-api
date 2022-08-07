e4d6116051477b72091a97a2b59acfc6bbf67488
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-08-07 17:53:52 +0400

    :rotating_light: Lints better

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,11 @@
-export { deepFreeze } from "./deepFreeze";
-export { isBrowser, isNode } from "./isBrowserOrNode";
-export { sleep } from "./sleep";
-export { spotifyFetch } from "./spotifyFetch";
-export { toBase64 } from "./toBase64";
-export { toURLString } from "./toURLString";
+export { deepFreeze } from './deepFreeze';
+export { isBrowser, isNode } from './isBrowserOrNode';
+export { sleep } from './sleep';
+export { spotifyFetch } from './spotifyFetch';
+export { toBase64 } from './toBase64';
+export { toURLString } from './toURLString';
 export type {
-	Image,
-	SpotifyPageURL,
-	SpotifyAPIURL
-} from "./SpotifyUtilityTypes";
+  Image,
+  SpotifyPageURL,
+  SpotifyAPIURL,
+} from './SpotifyUtilityTypes';
