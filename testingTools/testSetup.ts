3d3e43cb3e3a687f56658a8a4b7357eac66158fd
Eric Kwoka <43540491+ekwoka@users.noreply.github.com>
2022-10-12 19:04:45 +0400

    🔧 Updates Configs and Dependencies (#27)
    * :heavy_plus_sign: Adds lint-staged
    
    * :memo: Adds currently played to roadmap
    
    * :arrow_up: Updates dev deps
    
    * :wrench: Updates Vite Reporter

--- testingTools/testSetup.ts
+++ testingTools/testSetup.ts
@@ -1,5 +1,25 @@
-import { afterAll } from 'vitest';
+import { afterAll, beforeAll } from 'vitest';
 import { mockAgent } from './mockAgent';
 
 //  Close server after all tests
+beforeAll(() => {
+  const warningListeners = process.listeners('warning');
+
+  if (warningListeners.length != 1) {
+    console.warn(
+      `expected 1 listener on the process "warning" event, saw ${warningListeners.length}`
+    );
+  }
+
+  if (warningListeners[0]) {
+    const originalWarningListener = warningListeners[0];
+    process.removeAllListeners('warning');
+
+    process.prependListener('warning', (warning) => {
+      if (warning.name != 'ExperimentalWarning') {
+        originalWarningListener(warning);
+      }
+    });
+  }
+});
 afterAll(async () => await mockAgent.close());
